88c8f595a2cfdf2136344db525b159418bdeabcc
Sayul <sayulramirez@gmail.com>
2023-07-16 13:38:03 -0600

    Agregando el primer diseño a la ventana Menu y agregando la ventana Principal y configurando el menu

--- src/alura/com/conversor/Menu.java
+++ src/alura/com/conversor/Menu.java
@@ -5,10 +5,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
 
 public class Menu extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel PaneMenu;
 
 	/**
 	 * Launch the application.
@@ -30,12 +34,31 @@ public class Menu extends JFrame {
 	 * Create the frame.
 	 */
 	public Menu() {
+		setTitle("Menu de Conversiones");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setBounds(100, 100, 307, 398);
+		PaneMenu = new JPanel();
+		PaneMenu.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(contentPane);
+		setContentPane(PaneMenu);
+		PaneMenu.setLayout(null);
+		
+		JLabel labelTitulo = new JLabel("Elige el tipo de conversion a realizar");
+		labelTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		labelTitulo.setBounds(10, 35, 271, 26);
+		PaneMenu.add(labelTitulo);
+		
+		JLabel labelDos = new JLabel("Conversiones disponibles: ");
+		labelDos.setHorizontalAlignment(SwingConstants.CENTER);
+		labelDos.setBounds(10, 124, 134, 26);
+		PaneMenu.add(labelDos);
+		
+		JComboBox comboBoxSeleccion = new JComboBox();
+		comboBoxSeleccion.setBounds(10, 158, 271, 22);
+		PaneMenu.add(comboBoxSeleccion);
+		
+		JButton botonComenzar = new JButton("Comenzar conversion");
+		botonComenzar.setBounds(10, 298, 271, 23);
+		PaneMenu.add(botonComenzar);
 	}
-
 }
---
+++ src/alura/com/conversor/Principal.java
@@ -0,0 +1,82 @@
+package alura.com.conversor;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JButton;
+import javax.swing.JMenuItem;
+import javax.swing.SwingConstants;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+
+public class Principal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Principal frame = new Principal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Principal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 419, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JMenuBar menuBarPrincipal = new JMenuBar();
+		menuBarPrincipal.setBounds(0, 0, 403, 22);
+		contentPane.add(menuBarPrincipal);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Cambiar conversion");
+		menuBarPrincipal.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Limpiar");
+		menuBarPrincipal.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Salir");
+		menuBarPrincipal.add(mntmNewMenuItem_2);
+	}
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
+}
