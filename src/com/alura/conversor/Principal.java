6b375769c7faa0cac11cb2c8843bf761e876c0d6
Sayul <sayulramirez@gmail.com>
2023-07-21 00:54:11 -0600

    Comentado el método main de el archivo principal

--- src/com/alura/conversor/Principal.java
+++ src/com/alura/conversor/Principal.java
@@ -36,18 +36,18 @@ public class Principal extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Principal frame = new Principal();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					Principal frame = new Principal();
+//					frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
 
 	/**
 	 * Create the frame.
