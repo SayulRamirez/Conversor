2150f4f812d6c85e475169b876b291c38216d4d9
Sayul <sayulramirez@gmail.com>
2023-07-28 18:14:16 -0600

    Reorganizando el código

--- src/com/alura/logicaprincipal/Divisas.java
+++ src/com/alura/logicaprincipal/Divisas.java
@@ -16,6 +16,23 @@ public class Divisas {
 			"Yen Japonés",
 			"Won sur-coreano"
 	};
+
+	private static final String DOLAR_PESO = "16.81";
+	private static final String DOLAR_DOLAR = "1";
+	private static final String DOLAR_EURO = "0.91";
+	private static final String DOLAR_ESTERLINA = "0.78";
+	private static final String DOLAR_YEN = "139.04";
+	private static final String DOLAR_WONG = "1277.49";
+	
+	private String[] taza = {
+			"",
+			DOLAR_PESO,
+			DOLAR_DOLAR,
+			DOLAR_EURO,
+			DOLAR_ESTERLINA,
+			DOLAR_YEN,
+			DOLAR_WONG
+	};
 	
 	public void getDivisas(JComboBox<String> combo) {
 		for(String i: divisas) {
@@ -24,25 +41,17 @@ public class Divisas {
 	}
 	
 	
-	public void solucionar(JTextField t, String conversionInicial, String conversionFinal, double inicio) {
+	public void solucionarDivisas(JTextField t, String conversionInicial, String conversionFinal, double inicio) {
 		
-		if("Peso Mexicano".equals(conversionInicial)) {
-			if("Dólar".equals(conversionFinal)) {
-				resultado = TazaConversion.DOLAR_PESO.getValor() * inicio;				
-			} else if("Euros".equals(conversionFinal)) {
-				resultado = TazaConversion.DOLAR_EURO.getValor() * inicio;				
-			} else if("Libras Esterlinas".equals(conversionFinal)) {
-				resultado = TazaConversion.DOLAR_ESTERLINA.getValor() * inicio;				
-			} else if ("Yen Japonés".equals(conversionFinal)) {
-				resultado = TazaConversion.DOLAR_YEN.getValor() * inicio;				
-			}else if("Won sur-coreano".equals(conversionFinal)) {				
-				resultado = TazaConversion.DOLAR_WONG.getValor() * inicio;
+		for(int i = 1; i < divisas.length; i++) {
+			for(int j = 1; j < divisas.length; j++) {
+				
+				if(divisas[i].equals(conversionInicial) && divisas[j].equals(conversionFinal)) {
+					resultado = (inicio / Double.parseDouble(taza[i])) * Double.parseDouble(taza[j]);					
+				}
 			}
 		}
-//		"Dólar",
-//		"Euros",
-//		"Libras Esterlinas",
-//		"Yen Japonés",
-//		"Won sur-coreano"
+		
+		t.setText("Equivale a " + this.resultado + " " + conversionFinal); 
 	}
 }
