80ec704335010ab1a1d8e5426ab133e7567ac0ab
Sayul <sayulramirez@gmail.com>
2023-07-29 21:50:17 -0600

    Mejorando el código del archivo LogicaMenu.java

--- src/com/alura/logicaprincipal/LogicaMenu.java
+++ src/com/alura/logicaprincipal/LogicaMenu.java
@@ -7,8 +7,8 @@ public class LogicaMenu {
 	private String[] opciones = {"", "Divisas", "Temperaturas"};
 	
 	public void anadirDatos(JComboBox<String> combo) {
-		  for(int i = 0; i < opciones.length; i++) {
-				combo.addItem(opciones[i]);			
-			}
+		for(String i: opciones) {
+			combo.addItem(i);
+		}
 	  }
 }
