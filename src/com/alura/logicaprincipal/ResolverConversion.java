8ed2607bbee8c21cb8aca9ffdae5c6e7fc753ee4
Sayul <sayulramirez@gmail.com>
2023-08-07 00:02:11 -0600

    Añadida un interfaz ResolverConversion para implementar el método resolver en las diferentes clases que harán la conversión de unidades

---
+++ src/com/alura/logicaprincipal/ResolverConversion.java
@@ -0,0 +1,8 @@
+package com.alura.logicaprincipal;
+
+import javax.swing.JTextField;
+
+public interface ResolverConversion {
+
+	public void resolver(JTextField t, String conversionInicial, String conversionFinal, double inicio);
+}
